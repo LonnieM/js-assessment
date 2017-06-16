5750f35a21dff14acc848a6faf704024fa9475ff
Lonnie Minter <lonnie@Lonnies-MBP.attlocal.net>
2017-06-16 10:04:35 -0500

    some completed quiz questions

--- app/arrays.js
+++ app/arrays.js
@@ -2,11 +2,15 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
-
+    return arr.indexOf(item);
   },
 
   sum: function(arr) {
-
+    var sum = 0;
+    for (var i = 0, n = arr.length; i < n; i++) {
+      sum += arr[i];
+    }
+    return sum;
   },
 
   remove: function(arr, item) {
@@ -18,31 +22,42 @@ exports.arraysAnswers = {
   },
 
   append: function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate: function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend: function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail: function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat: function(arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert: function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count: function(arr, item) {
-
+    var count = 0;
+    for (var i = 0, n = arr.length; i < n; i++) {
+      if (arr[i] === item) {
+        count++;
+      }
+    }
+    return count;
   },
 
   duplicates: function(arr) {
@@ -50,7 +65,13 @@ exports.arraysAnswers = {
   },
 
   square: function(arr) {
+    sqr= [];
 
+    for (var i = 0, len = arr.length; i < len; i++) {
+        sqr.push(arr[i] * arr[i]);
+    }
+
+    return sqr;
   },
 
   findAllOccurrences: function(arr, target) {
